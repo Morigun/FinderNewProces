4892df4238c61b62db01aa89410f1f6dde7a4292
Morigun <GunGrayv@gmail.com>
2015-03-09 10:24:35 +0300

    Create

---
+++ FinderNewProces/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FinderNewProces
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            GetProcess();
+        }
+        
+        private void GetProcess()
+        {
+            listBox1.Items.Clear();
+            foreach (Process p in Process.GetProcesses())
+                listBox1.Items.Add(p.ToString() +" "+ p.ProcessName);
+        }
+
+        private void Sorted()
+        {
+            
+        }
+    }
+}
